3a356d1b5bbdf017be98bc193e1435717e4357bf
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2018-07-30 16:24:38 +1000

    Potential Fix 2

--- src/main.ino
+++ src/main.ino
@@ -1,17 +1,18 @@
 #include <Arduino.h>
 
-int motorPin = 5;
-int bxco = 150;
-int byco = 120;
+
 
 void setup(){
-pinMode(motorPin, OUTPUT);
+  //int motorPin = 5;
+  int bxco = 150;
+  int byco = 120;
+//pinMode(motorPin, OUTPUT);
 
 
 }
 
 void loop(){
-analogWrite(motorPin, 255);
+//analogWrite(motorPin, 255);
 
 
 
@@ -22,23 +23,32 @@ analogWrite(motorPin, 255);
 if (bxco >= 160) and (bxco <= 240){
     if (byco < 80){
       //Ball is at the front
+      print("Test 1")
 }   if (byco <= 160) and (byco >= 80){
+      print("Test 2")
       //Ball is literally on the camera
 }   if (byco > 160){
       //Ball is at the back
+      print("Test 3")
 }if (bxco > 240){
 }   if (byco < 80){
+      print("Test 4")
       //Ball is to the front right
 }   if (byco <= 160) and (byco >= 80){
+      print("Test 5")
       //Ball is on the right
 }   if (byco > 160){
+      print("Test 6")
       //Ball is to the back right
 }if (bxco < 160){
 }   if (byco < 80){
+      print("Test 7")
       //Ball is to the front left
 }   if (byco <= 160) and (byco >= 80){
+      print("Test 8")
       //Ball is on the left
 }   if (byco > 160){
+      print("Test 9")
       //Ball is to the back left
       }
     }
