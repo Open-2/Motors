71da00c19e418a42eb3c173495cb7cd9e018fc42
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2018-07-30 16:33:39 +1000

    Fixed but created another bug, fix that

--- src/main.ino
+++ src/main.ino
@@ -12,7 +12,7 @@ void setup(){
 }
 
 void loop(){
-analogWrite(motorPin, 255);
+  analogWrite(motorPin, 255);
 
 
 
@@ -23,32 +23,23 @@ analogWrite(motorPin, 255);
 if (bxco >= 160 && bxco <= 240){
     if (byco < 80){
       //Ball is at the front
-      print("Test 1")
 }   if (byco <= 160 && byco >= 80){
-      print("Test 2")
       //Ball is literally on the camera
 }   if (byco > 160){
       //Ball is at the back
-      print("Test 3")
 }if (bxco > 240){
 }   if (byco < 80){
-      print("Test 4")
       //Ball is to the front right
 }   if (byco <= 160 && byco >= 80){
-      print("Test 5")
       //Ball is on the right
 }   if (byco > 160){
-      print("Test 6")
       //Ball is to the back right
 }if (bxco < 160){
 }   if (byco < 80){
-      print("Test 7")
       //Ball is to the front left
 }   if (byco <= 160 && byco >= 80){
-      print("Test 8")
       //Ball is on the left
 }   if (byco > 160){
-      print("Test 9")
       //Ball is to the back left
       }
     }
