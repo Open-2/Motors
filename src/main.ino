2e27a0859bd4b901b9d39df8578135b0619a8053
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2018-07-30 16:26:42 +1000

    Fuck this

--- src/main.ino
+++ src/main.ino
@@ -23,32 +23,32 @@ void loop(){
 if (bxco >= 160) and (bxco <= 240){
     if (byco < 80){
       //Ball is at the front
-      print("Test 1")
+      print("Test 1");
 }   if (byco <= 160) and (byco >= 80){
-      print("Test 2")
+      print("Test 2");
       //Ball is literally on the camera
 }   if (byco > 160){
       //Ball is at the back
-      print("Test 3")
+      print("Test 3");
 }if (bxco > 240){
 }   if (byco < 80){
-      print("Test 4")
+      print("Test 4");
       //Ball is to the front right
 }   if (byco <= 160) and (byco >= 80){
-      print("Test 5")
+      print("Test 5");
       //Ball is on the right
 }   if (byco > 160){
-      print("Test 6")
+      print("Test 6");
       //Ball is to the back right
 }if (bxco < 160){
 }   if (byco < 80){
-      print("Test 7")
+      print("Test 7");
       //Ball is to the front left
 }   if (byco <= 160) and (byco >= 80){
-      print("Test 8")
+      print("Test 8");
       //Ball is on the left
 }   if (byco > 160){
-      print("Test 9")
+      print("Test 9");
       //Ball is to the back left
       }
     }
