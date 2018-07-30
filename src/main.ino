2cdd696df2d56d5e1f17a184e43a9f2a25fc1d72
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2018-07-30 16:10:15 +1000

    Added Motor Coordinate Test
    Added positional reactions for motors

--- src/main.ino
+++ src/main.ino
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 
 int motorPin = 5;
+int bxco = 150;
+int
 
 void setup(){
 pinMode(motorPin, OUTPUT);
@@ -11,4 +13,33 @@ pinMode(motorPin, OUTPUT);
 void loop(){
 analogWrite(motorPin, 255);
 
+
+
+
+
+
+
+if (bxco >= 160) and (bxco <= 240){
+    if (byco < 80){
+      //Ball is at the front
+}   if (byco <= 160) and (byco >= 80){
+      //Ball is literally on the camera
+}   if (byco > 160){
+      //Ball is at the back
+}if (bxco > 240){
+}  if (byco < 80){
+      //Ball is to the front right
+}   if (byco <= 160) and (byco >= 80){
+      //Ball is on the right
+}   if (byco > 160){
+      //Ball is to the back right
+}if (bxco < 160){
+}   if (byco < 80){
+      //Ball is to the front left
+}    if (byco <= 160) and (byco >= 80){
+      //Ball is on the left
+}   if (byco > 160){
+      //Ball is to the back left
+      }
+    }
 }
